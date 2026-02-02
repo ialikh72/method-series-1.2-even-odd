243405d817431f34a4164a781f9b4a20381707c4
ialikh72 <ialikh.w1@gmail.com>
2026-02-02 13:04:27 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Evv
+{
+    static void Main()
+    {
+       int num= number();
+        Console.WriteLine(num);
+        
+    }
+    static int number()
+    {
+        Console.WriteLine("enter a number to check");
+        int num = int.Parse(Console.ReadLine());
+        if ((num / 2) * 2 == num)
+        {
+            Console.WriteLine("num is even");
+            return num;
+        }
+        else
+        {
+            Console.WriteLine("num is odd");
+            return num;
+        }
+    }
+}
